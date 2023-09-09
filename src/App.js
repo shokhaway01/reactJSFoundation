bfe7d24f4d0bf473c32252e9c1df699d523d7d1b
shokhaway01 <shoxauey@gmail.com>
2023-09-09 21:09:16 +0500

    few files were added and few files were removed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import {IoPersonSharp, IoSettingsSharp} from "react-icons/io5";
-import {BiSolidMessageRounded, BiSolidMusic, BiSolidNews} from "react-icons/bi";
-import {Header} from "./components/Header/Header";
-import {Nav} from "./components/Nav/Nav";
-import {Profile} from "./components/Profile/Profile";
 
 function App() {
     return (
         <div className='app-wrapper'>
-            <Header/>
-            <Nav/>
-            <Profile/>
+
         </div>
     );
  }
